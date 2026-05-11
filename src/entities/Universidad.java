52435ee135bc20f7d2b61132764527a3ac749b45
Diego Ruiz <diego.ruiz@usantoto.edu.co>
2026-05-11 17:23:55 -0500

    Cree la entidad universidad

--- src/entities/Universidad.java
+++ src/entities/Universidad.java
@@ -1,55 +1,55 @@
-package entities;
+ package entities;
 
-import java.util.List;
+ import java.util.List;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+ import javax.persistence.Entity;
+ import javax.persistence.GeneratedValue;
+ import javax.persistence.GenerationType;
+ import javax.persistence.Id;
+ import javax.persistence.OneToMany;
+ import javax.persistence.Table;
 
-@Entity
-@Table(name = "universidad")
-public class Universidad {
+ @Entity
+ @Table(name = "universidad")
+ public class Universidad {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idUniversidad;
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private Long idUniversidad;
 
-    private String nombre;
+     private String nombre;
 
-    @OneToMany(mappedBy = "universidad")
-    private List<Programa> programas;
+     @OneToMany(mappedBy = "universidad")
+     private List<Programa> programas;
 
-    public Universidad() {
-    }
+     public Universidad() {
+     }
 
-    public Universidad(String nombre) {
-        this.nombre = nombre;
-    }
+     public Universidad(String nombre) {
+         this.nombre = nombre;
+     }
 
-    public Long getIdUniversidad() {
-        return idUniversidad;
-    }
+     public Long getIdUniversidad() {
+         return idUniversidad;
+     }
 
-    public void setIdUniversidad(Long idUniversidad) {
-        this.idUniversidad = idUniversidad;
-    }
+     public void setIdUniversidad(Long idUniversidad) {
+         this.idUniversidad = idUniversidad;
+     }
 
-    public String getNombre() {
-        return nombre;
-    }
+     public String getNombre() {
+         return nombre;
+     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+     public void setNombre(String nombre) {
+         this.nombre = nombre;
+     }
 
-    public List<Programa> getProgramas() {
-        return programas;
-    }
+     public List<Programa> getProgramas() {
+         return programas;
+     }
 
-    public void setProgramas(List<Programa> programas) {
-        this.programas = programas;
-    }
-}
+     public void setProgramas(List<Programa> programas) {
+         this.programas = programas;
+     }
+ }
