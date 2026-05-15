a9e935e41f25414b4d59cce587406ac00c1ed9da
Diego Ruiz <diego.ruiz@usantoto.edu.co>
2026-05-15 15:11:29 -0500

    Cree algunos daos

--- src/entities/Estudiante.java
+++ src/entities/Estudiante.java
@@ -1,40 +1,57 @@
+package entities;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+@Entity
+@Table(name = "estudiante")
 public class Estudiante {
 
-    @Entity
-    @Table(name = " estudiante")
-    public class Estudiante {
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long idEstudiante;
-        private String nombre;
-        private String apellido;
-        private String documento;
-        private int semestre;
-        private boolean induccion;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEstudiante;
 
-        @manyToOne
-        @joinColumn(name = "id:programa")
-        private Programa Programa;
-        @oneToMany(mappedBy = "estudiante")
-        private List<RegistroIngreso> registos;
+    private String nombres;
+    private String apellidos;
+    private String documento;
+    private int semestre;
 
-        
-    }
+    private LocalDate fechaInduccion;
+    private LocalDate arlVencimiento;
+
+    @ManyToOne
+    @JoinColumn(name = "id_programa")
+    private Programa programa;
 
     public Estudiante() {
     }
 
-    public Estudiante(String nombre, String apellido, String documento, int semestre, boolean induccion) {
-        this.nombre = nombre;
-        this.apellido = apellido;
+    public Estudiante(String nombres,
+                      String apellidos,
+                      String documento,
+                      int semestre,
+                      LocalDate fechaInduccion,
+                      LocalDate arlVencimiento,
+                      Programa programa) {
+
+        this.nombres = nombres;
+        this.apellidos = apellidos;
         this.documento = documento;
         this.semestre = semestre;
-        this.induccion = induccion;
+        this.fechaInduccion = fechaInduccion;
+        this.arlVencimiento = arlVencimiento;
+        this.programa = programa;
     }
-      public Long getIdEstudiante() {
+
+    public Long getIdEstudiante() {
         return idEstudiante;
     }
 
@@ -74,12 +91,20 @@ public class Estudiante {
         this.semestre = semestre;
     }
 
-    public boolean isInduccion() {
-        return induccion;
+    public LocalDate getFechaInduccion() {
+        return fechaInduccion;
     }
 
-    public void setInduccion(boolean induccion) {
-        this.induccion = induccion;
+    public void setFechaInduccion(LocalDate fechaInduccion) {
+        this.fechaInduccion = fechaInduccion;
+    }
+
+    public LocalDate getArlVencimiento() {
+        return arlVencimiento;
+    }
+
+    public void setArlVencimiento(LocalDate arlVencimiento) {
+        this.arlVencimiento = arlVencimiento;
     }
 
     public Programa getPrograma() {
@@ -88,13 +113,5 @@ public class Estudiante {
 
     public void setPrograma(Programa programa) {
         this.programa = programa;
-    }
-
-    public List<RegistroIngreso> getRegistros() {
-        return registros;
-    }
-
-    public void setRegistros(List<RegistroIngreso> registros) {
-        this.registros = registros;
     }
 }
